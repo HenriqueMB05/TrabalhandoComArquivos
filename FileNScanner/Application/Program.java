c14a6d880d7f77c17096c802c5408825c0ddf73d
Henrique <matosh38@gmail.com>
2025-03-22 16:49:25 -0300

    Bloco try-with-resources

--- FileNScanner/Application/Program.java
+++ FileNScanner/Application/Program.java
@@ -6,34 +6,18 @@ import java.io.IOException;
 
 public class Program  {
     public static void main(String[] args){
-        FileReader fr = null;
-        BufferedReader br =null;
-
         String path = "c:\\temp\\in.txt";
 
-        try{
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
             String line = br.readLine();
 
-            while(line !=null){
+            while(line!=null){
                 System.out.println(line);
                 line = br.readLine();
             }
+
         } catch (IOException err){
             System.out.println("Error: "+ err.getMessage());
-        } finally {
-            try{
-                if(br !=null){
-                    br.close();
-                } if(fr !=null){
-                    fr.close();
-                }
-            } catch (IOException err){
-                err.printStackTrace();
-            }
         }
-
     }
 }
