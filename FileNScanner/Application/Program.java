2993d3e563011a1b0b922e87d174e818285985a8
Henrique <matosh38@gmail.com>
2025-03-22 19:59:25 -0300

    Manipulando diretorio

--- FileNScanner/Application/Program.java
+++ FileNScanner/Application/Program.java
@@ -1,23 +1,28 @@
 package FileNScanner.Application;
 
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program  {
     public static void main(String[] args){
-        String[] list = new String[]{"Good mornig", "Good Afternoon", "Good night"};
+        Scanner sc = new Scanner(System.in);
 
-        String path = "c:\\temp\\out.txt";
+        System.out.print("Enter a folder path: ");
+        String strPath = sc.nextLine();
 
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
-            for (String line : list){
-                bw.write(line);
-                bw.newLine();
-            }
-        } catch (IOException err){
-            System.out.println("Error: "+ err.getMessage());
+        File path = new File(strPath);
+
+        File[] folders = path.listFiles(File::isDirectory);
+
+        for(File folder : folders){
+            System.out.println(folder);
         }
+
+        boolean success = new File(strPath+ "\\subdir").mkdir();
+        System.out.println("Directory created successfully: "+ success);
+
+
+
+        sc.close();
     }
 }
